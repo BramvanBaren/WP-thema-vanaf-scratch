ae1ff1e10b410dc851b9d2fa2a8ffa808eb61c38
BramvanBaren <55235762+BramvanBaren@users.noreply.github.com>
2020-07-06 11:42:33 +0200

    Add files via upload

--- functions.php
+++ functions.php
@@ -5,3 +5,12 @@ function laadStijlblad() {
 }
 
 add_action('init', 'laadStijlblad');
+
+
+function registreer_menu() {
+    $argumenten = array(
+        'hoofd-menu' => __('Hoofd menu')
+    );
+    register_nav_menus();
+}
+add_action('init', 'registreer_menu');
